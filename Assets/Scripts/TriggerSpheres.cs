eb7dbcf77f49209266f303252911a1d1fcc4ea60
Bikram Kumar <coolbikram0@gmail.com>
2022-05-29 16:41:23 +0530

    game complete

--- Assets/Scripts/TriggerSpheres.cs
+++ Assets/Scripts/TriggerSpheres.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class TriggerSpheres : MonoBehaviour
 {
     public float triggerSpeed = 5f;
+
+
     void OnTriggerEnter(Collider collider)
     {
-        if (collider.name == "Player")
+        if(collider.name == "Player")
         {
-            foreach (GameObject sphere in GameObject.FindGameObjectsWithTag("SphericalObstacle"))
+            Transform sphericalObstacles = transform.parent.Find("SphericalObstacles").transform;
+
+            for (int i = 0; i < sphericalObstacles.childCount; i++) 
             {
+                GameObject sphere = sphericalObstacles.GetChild(i).gameObject;
                 Vector3 displacementUnitVec = (transform.position - sphere.transform.position).normalized;
                 sphere.GetComponent<Rigidbody>().AddForce(displacementUnitVec * triggerSpeed , ForceMode.VelocityChange);
             }
